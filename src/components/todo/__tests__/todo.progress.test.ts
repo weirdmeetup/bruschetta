1e8320309fd95260aee689ec28c347b8fc28f408
totuworld <totuworld@gmail.com>
2019-04-05 21:49:24 +0900

    jest junit 추가
    build pipeline 용

--- src/components/todo/__tests__/todo.progress.test.ts
+++ src/components/todo/__tests__/todo.progress.test.ts
@@ -5,7 +5,7 @@ import { IIssueWithID } from '../interface/IIssue';
 import { calProgress } from '../services/cal_progress';
 
 /*
-2 epic, 3 depth
+4 epic, 3 depth
 desc 1
 # epic (1)
 [ ] parent 1 (1)
@@ -19,19 +19,21 @@ desc 1
 # epic (2)
   [x] parent 2_1
   [x] parent 2_2
+# epic (1)
+[x] parent 3_1
  */
 
 const mock: IIssueWithID[] = [
   { id: 'desc01', text: 'desc', type: EN_ISSUE_TYPE.EPIC, depth: 0 },
   { id: 'epic01', text: '# epic', type: EN_ISSUE_TYPE.EPIC, depth: 0 },
-  { id: 'parent01', text: '[ ] parent 1', type: EN_ISSUE_TYPE.TASK, depth: 1 },
-  { id: 'p01_c01', text: '[ ] child', type: EN_ISSUE_TYPE.TASK, depth: 2 },
-  { id: 'p01_c02', text: '[x] child', type: EN_ISSUE_TYPE.TASK, depth: 2 },
-  { id: 'parent02', text: '[x] parent 2', type: EN_ISSUE_TYPE.TASK, depth: 1 },
-  { id: 'p02_c01', text: '[ ] child', type: EN_ISSUE_TYPE.TASK, depth: 2 },
-  { id: 'p02_c02', text: '[x] child', type: EN_ISSUE_TYPE.TASK, depth: 2 },
-  { id: 'p02_c02_c01', text: '[x] child', type: EN_ISSUE_TYPE.TASK, depth: 3 },
-  { id: 'p02_c03', text: '[ ] child', type: EN_ISSUE_TYPE.TASK, depth: 2 },
+  { id: 'parent01', text: '[ ] parent 1', type: EN_ISSUE_TYPE.TASK, depth: 0 },
+  { id: 'p01_c01', text: '[ ] child', type: EN_ISSUE_TYPE.TASK, depth: 1 },
+  { id: 'p01_c02', text: '[x] child', type: EN_ISSUE_TYPE.TASK, depth: 1 },
+  { id: 'parent02', text: '[x] parent 2', type: EN_ISSUE_TYPE.TASK, depth: 0 },
+  { id: 'p02_c01', text: '[ ] child', type: EN_ISSUE_TYPE.TASK, depth: 1 },
+  { id: 'p02_c02', text: '[x] child', type: EN_ISSUE_TYPE.TASK, depth: 1 },
+  { id: 'p02_c02_c01', text: '[x] child', type: EN_ISSUE_TYPE.TASK, depth: 2 },
+  { id: 'p02_c03', text: '[ ] child', type: EN_ISSUE_TYPE.TASK, depth: 1 },
   { id: 'epic02', text: '# epic', type: EN_ISSUE_TYPE.EPIC, depth: 0 },
   {
     id: 'parent02_01',
@@ -44,6 +46,13 @@ const mock: IIssueWithID[] = [
     text: '[x] parent 2_2',
     type: EN_ISSUE_TYPE.TASK,
     depth: 1
+  },
+  { id: 'epic03', text: '# epic', type: EN_ISSUE_TYPE.EPIC, depth: 0 },
+  {
+    id: 'parent03_01',
+    text: '[x] parent 3_1',
+    type: EN_ISSUE_TYPE.TASK,
+    depth: 0
   }
 ];
 
@@ -62,6 +71,14 @@ describe('진행율 계산 테스트', () => {
     expect(testData).toBeTruthy();
     expect(testData!.total).toEqual(2);
     expect(testData!.done).toEqual(2);
+    done();
+  });
+  test('계산 테스트 - epic03 (total1, done1)', done => {
+    const result = calProgress(mock);
+    const testData = result.get('epic03');
+    expect(testData).toBeTruthy();
+    expect(testData!.total).toEqual(1);
+    expect(testData!.done).toEqual(1);
     done();
   });
   test('계산 테스트 - parent01 (total2, done1)', done => {
